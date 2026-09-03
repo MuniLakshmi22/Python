b0846e3edbb6a79865dc8229c4fa6d8335a6a841
MuniLakshmi22 <kalathurumunilakshmi22@gmail.com>
2026-09-03 20:05:42 +0530

    Enhance day-1.py with output examples and user input
    Added output comments for various data types and type conversions. Included user input handling for name, age, and salary.

--- day-1.py
+++ day-1.py
@@ -11,6 +11,12 @@ print("Variables")
 print("Name:", name)
 print("Age:", age)
 print("Student:", is_student)
+#------------------------------------------------------------
+#output
+#Variables
+#Name: Lakshmi
+#Age: 20
+#Student: True
 
 # ============================================================
 # 2. DATA TYPES
@@ -21,6 +27,11 @@ print("Student:", is_student)
 print("\nInteger")
 print("Value:", age)
 print("Type:", type(age))
+#------------------------------------------------------------
+#output
+#Integer
+#Value: 20
+#Type: <class 'int'>
 
 # ------------------------------------------------------------
 # float
@@ -29,6 +40,11 @@ price = 250.50
 print("\nFloat")
 print("Value:", price)
 print("Type:", type(price))
+#-------------------------------------------------------------
+#output
+#Float
+#Value: 250.5
+#Type: <class 'float'>
 
 # ------------------------------------------------------------
 # complex
@@ -37,6 +53,11 @@ number = 3 + 4j
 print("\nComplex")
 print("Value:", number)
 print("Type:", type(number))
+#------------------------------------------------------------
+#output
+#Complex
+#Value: (3+4j)
+#Type: <class 'complex'>
 
 # ------------------------------------------------------------
 # string
@@ -44,6 +65,11 @@ print("Type:", type(number))
 print("\nString")
 print("Value:", name)
 print("Type:", type(name))
+#------------------------------------------------------------
+#output
+#String
+#Value: Lakshmi
+#Type: <class 'str'>
 
 # ------------------------------------------------------------
 # boolean
@@ -51,6 +77,11 @@ print("Type:", type(name))
 print("\nBoolean")
 print("Value:", is_student)
 print("Type:", type(is_student))
+#------------------------------------------------------------
+#output
+#Boolean
+#Value: True
+#Type: <class 'bool'>
 
 # ------------------------------------------------------------
 # list
@@ -59,6 +90,11 @@ fruits = ["Apple", "Banana", "Mango"]
 print("\nList")
 print("Value:", fruits)
 print("Type:", type(fruits))
+#------------------------------------------------------------
+#output
+#List
+#Value: ['Apple', 'Banana', 'Mango']
+#Type: <class 'list'>
 
 # ------------------------------------------------------------
 # tuple
@@ -67,6 +103,11 @@ colors = ("Red", "Green", "Blue")
 print("\nTuple")
 print("Value:", colors)
 print("Type:", type(colors))
+#------------------------------------------------------------
+#output
+#Tuple
+#Value: ('Red', 'Green', 'Blue')
+#Type: <class 'tuple'>
 
 # ------------------------------------------------------------
 # set
@@ -75,6 +116,11 @@ numbers = {10, 20, 30}
 print("\nSet")
 print("Value:", numbers)
 print("Type:", type(numbers))
+#------------------------------------------------------------
+#output
+#Set
+#Value: {10, 20, 30}
+#Type: <class 'set'>
 
 # ------------------------------------------------------------
 # dictionary
@@ -85,6 +131,11 @@ student = {
 print("\nDictionary")
 print("Value:", student)
 print("Type:", type(student))
+#------------------------------------------------------------
+#output
+#Dictionary
+#Value: {'course': 'Python'}
+#Type: <class 'dict'>
 
 # ============================================================
 # 3. TYPE CONVERSION
@@ -97,6 +148,11 @@ number = int(number)
 print("\nString to Integer")
 print("Value:", number)
 print("Type:", type(number))
+#------------------------------------------------------------
+#output
+#String to Integer
+#Value: 100
+#Type: <class 'int'>
 
 # ------------------------------------------------------------
 # Integer -> Float
@@ -105,6 +161,11 @@ number = float(number)
 print("\nInteger to Float")
 print("Value:", number)
 print("Type:", type(number))
+#------------------------------------------------------------
+#output
+#Integer to Float
+#Value: 100.0
+#Type: <class 'float'>
 
 # ------------------------------------------------------------
 # Float -> Integer
@@ -113,6 +174,11 @@ price = int(price)
 print("\nFloat to Integer")
 print("Value:", price)
 print("Type:", type(price))
+#------------------------------------------------------------
+#output
+#Float to Integer
+#Value: 250
+#Type: <class 'int'>
 
 # ------------------------------------------------------------
 # Integer -> String
@@ -121,6 +187,11 @@ number = str(number)
 print("\nInteger to String")
 print("Value:", number)
 print("Type:", type(number))
+#------------------------------------------------------------
+#output
+#Integer to String
+#Value: 100.0
+#Type: <class 'str'>
 
 # ------------------------------------------------------------
 # String -> Float
@@ -129,6 +200,11 @@ price = float(price)
 print("\nString to Float")
 print("Value:", price)
 print("Type:", type(price))
+#------------------------------------------------------------
+#output
+#String to Float
+#Value: 250.0
+#Type: <class 'float'>
 
 # ------------------------------------------------------------
 # Integer -> Boolean
@@ -138,6 +214,11 @@ result = bool(number)
 print("\nInteger to Boolean")
 print("Value:", result)
 print("Type:", type(result))
+#------------------------------------------------------------
+#output
+#Integer to Boolean
+#Value: True
+#Type: <class 'bool'>
 
 # ------------------------------------------------------------
 # String -> Boolean
@@ -147,6 +228,11 @@ result = bool(text)
 print("\nString to Boolean")
 print("Value:", result)
 print("Type:", type(result))
+#------------------------------------------------------------
+#output
+#String to Boolean
+#Value: True
+#Type: <class 'bool'>
 
 # ------------------------------------------------------------
 # List -> Tuple
@@ -155,6 +241,11 @@ result = tuple(fruits)
 print("\nList to Tuple")
 print("Value:", result)
 print("Type:", type(result))
+#------------------------------------------------------------
+#output
+#List to Tuple
+#Value: ('Apple', 'Banana', 'Mango')
+#Type: <class 'tuple'>
 
 # ------------------------------------------------------------
 # Tuple -> List
@@ -163,6 +254,11 @@ result = list(colors)
 print("\nTuple to List")
 print("Value:", result)
 print("Type:", type(result))
+#-------------------------------------------------------------
+#output
+#Tuple to List
+#Value: ['Red', 'Green', 'Blue']
+#Type: <class 'list'>
 
 # ------------------------------------------------------------
 # List -> Set
@@ -172,6 +268,11 @@ result = set(numbers)
 print("\nList to Set")
 print("Value:", result)
 print("Type:", type(result))
+#------------------------------------------------------------
+#output
+#List to Set
+#Value: {10, 20, 30}
+#Type: <class 'set'>
 
 # ============================================================
 # 4. INPUT WITH TYPE CONVERSION
@@ -180,7 +281,6 @@ print("Type:", type(result))
 name = input("\nEnter your name: ")
 age = int(input("Enter your age: "))
 salary = float(input("Enter your salary: "))
-
 print("\nUser Details")
 print("Name:", name)
 print("Age:", age)
@@ -193,3 +293,17 @@ print("\nData Types")
 print("Name Type:", type(name))
 print("Age Type:", type(age))
 print("Salary Type:", type(salary))
+#----------------------------------------------------------------
+#output
+#User Details
+#Name: lucky
+#Age: 20
+#Salary: 5000000.0
+#Data Types
+#Name Type: <class 'str'>
+#Age Type: <class 'int'>
+#Salary Type: <class 'float'>
+#Data Types
+#Name Type: <class 'str'>
+#Age Type: <class 'int'>
+#Salary Type: <class 'float'>
